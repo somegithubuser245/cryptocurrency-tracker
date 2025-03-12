adbfcf7cf203127252e1aa58c1db7b5c06022d67
somegithubuser245 <New_username_001@protonmail.com>
2025-03-12 15:47:26 +0100

    initial changes
    - just trying out cleaner code
    - working tradingview chart
    - still very unsafe and not dynamic at all

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,7 @@
-import CryptoChart from './components/CryptoChart'
+import Chart from "./components/Chart";
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <div className="container mx-auto px-4">
-        <h1 className="text-3xl font-bold text-gray-900 mb-8">
-          Crypto Price Tracker
-        </h1>
-        <CryptoChart />
-      </div>
-    </div>
-  )
+  return <Chart></Chart>
 }
 
 export default App
