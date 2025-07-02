1a89c0a69a2302ff84fa18d85365eea6543c0511
somegithubuser245 <New_username_001@protonmail.com>
2025-07-02 20:39:44 +0200

    Working dict generation. Added endpoint in BE

--- backend/src/routes/static_data.py
+++ backend/src/routes/static_data.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from routes.models.schemas import config_types
+from services.external_api_caller import CryptoFetcher
+
+crypto_fetcher = CryptoFetcher()
 
 static_router = APIRouter(prefix="/static")
 
@@ -7,3 +10,8 @@ static_router = APIRouter(prefix="/static")
 @static_router.get("/config/{config_type}")
 def get_config(config_type: str) -> dict:
     return config_types.get(config_type)
+
+
+@static_router.get("/pairs-exchanges")
+async def get_pairs_exchanges_dict() -> dict:
+    return await crypto_fetcher.get_arbitrable_pairs()
--- backend/src/services/external_api_caller.py
+++ backend/src/services/external_api_caller.py
@@ -1,6 +1,8 @@
 import asyncio
 
 import ccxt.async_support as ccxt
+from config.config import SUPPORTED_EXCHANGES
+from config.config import Exchange as ExchangeEnum
 from routes.models.schemas import PriceTicketRequest
 
 
@@ -13,13 +15,55 @@ class CryptoFetcher:
         self._exchanges: dict[str, ccxt.Exchange] = {}
 
     async def get_ohlc(self, request: PriceTicketRequest) -> list[list[float]]:
-        exchange = self.get_exchange(request.api_provider.value)
+        exchange = self.get_saved_exchange(request.api_provider.value)
         return await exchange.fetch_ohlcv(
             request.crypto_id.replace("-", "/"),
             request.interval,
         )
 
-    def get_exchange(self, exchange: str) -> ccxt.Exchange:
+    async def get_arbitrable_pairs(self) -> dict[str, list[str]]:
+        """
+        Generate a dict for arbitrable pairs
+
+        Returns
+        ----
+        dictionary containing Pair as a key and list of Exchanges as a value
+
+        Example:
+        ```
+        {
+        "BTC-USDT": ["Binance", "okx", "mexc", "bingx"],
+        "DOGE-USDT": ["bingx", "okx"],
+        ...
+        }
+        ```
+        """
+        exchanges = [self.get_saved_exchange(exchange) for exchange in SUPPORTED_EXCHANGES.values()]
+
+        # load markets to be able to access .symbols of each exchange
+        await asyncio.gather(*[exchange.load_markets() for exchange in exchanges])
+
+        # TODO: use pandas for quicker data preprocessing
+        all_symbols = [symbol for exchange in exchanges for symbol in exchange.symbols]
+        unique_symbols = list(set(all_symbols))
+        arbitrable_symbols = sorted(
+            [symbol for symbol in unique_symbols if all_symbols.count(symbol) > 1]
+        )
+
+        pairs_exchanges_dict: dict[str, list[ExchangeEnum]] = {}
+        for symbol in arbitrable_symbols:
+            for exchange in exchanges:
+                exchange_name = exchange.id
+                if symbol not in exchange.symbols:
+                    continue
+                if pairs_exchanges_dict.get(symbol):
+                    pairs_exchanges_dict[symbol].append(exchange_name)
+                else:
+                    pairs_exchanges_dict[symbol] = [exchange_name]
+
+        return pairs_exchanges_dict
+
+    def get_saved_exchange(self, exchange: str) -> ccxt.Exchange:
         if exchange not in self._exchanges:
             self._exchanges[exchange] = self.get_ccxt_exchange(exchange)
         return self._exchanges[exchange]
