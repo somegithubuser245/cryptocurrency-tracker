48a124e4f93178a64a373af9410b791b3c927ded
somegithubuser245 <New_username_001@protonmail.com>
2025-07-24 13:02:04 +0200

    Use bools dataframes instead of strings

--- backend/src/services/external_api_caller.py
+++ backend/src/services/external_api_caller.py
@@ -1,8 +1,8 @@
 import asyncio
 
 import ccxt.async_support as ccxt
-import pandas as pd
 import numpy as np
+import pandas as pd
 from config.config import SUPPORTED_EXCHANGES
 from routes.models.schemas import PriceTicketRequest
 
@@ -22,7 +22,7 @@ class CryptoFetcher:
             request.interval,
         )
 
-    async def get_arbitrable_pairs(self) -> dict[str, list[str]]:
+    async def get_arbitrable_pairs(self) -> dict[str, dict[str, bool]]:
         """
         Generate a dict for arbitrable pairs
 
@@ -45,32 +45,29 @@ class CryptoFetcher:
         await asyncio.gather(*[exchange.load_markets() for exchange in exchanges])
 
         symbols_frames_raw = [
-            pd.DataFrame({exchange.id: exchange.symbols}) for exchange in exchanges
+            pd.DataFrame({exchange.id: np.True_}, index=exchange.symbols, dtype=np.bool)
+            for exchange in exchanges
         ]
 
         left_merge_frame = symbols_frames_raw[0]
 
         for right_merge_frame in symbols_frames_raw[1:]:
-            left_name = left_merge_frame.columns[-1]
-            right_name = right_merge_frame.columns[0]
-
             left_merge_frame = left_merge_frame.merge(
-                right_merge_frame,
-                how="outer",
-                left_on=left_name,
-                right_on=right_name,
+                right_merge_frame, how="outer", left_index=True, right_index=True
             )
-
-            print(left_merge_frame.dropna().head())
 
         # drop only if found on single exchange
         threshold = 2
         left_merge_frame.dropna(thresh=threshold, inplace=True)
 
-        print(left_merge_frame.transpose())
-        return left_merge_frame.to_dict(
-            orient="index"
+        # convert to desired format
+        left_merge_frame.fillna(False, inplace=True)
+        supported_exchanges_list_like = left_merge_frame.apply(
+            # x[x] is equivalent to "select all x, where x is True"
+            lambda x: x[x].index.tolist(), axis=1
         )
+
+        return supported_exchanges_list_like.to_dict()
 
     def get_saved_exchange(self, exchange: str) -> ccxt.Exchange:
         if exchange not in self._exchanges:
