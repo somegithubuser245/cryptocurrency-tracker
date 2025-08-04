e001da5e17573f3fad0d782c7caa3b16b36a4b04
somegithubuser245 <New_username_001@protonmail.com>
2025-08-05 00:11:51 +0200

    minor cleanup

--- backend/src/routes/scan_spreads.py
+++ backend/src/routes/scan_spreads.py
@@ -2,21 +2,8 @@ from fastapi import APIRouter
 
 from routes.models.schemas import PriceTickerRequest
 from services.dependencies import spreads_calculator_dependency
-from data_handling.spread_object import Spread
 
 spreads_router = APIRouter(prefix="/spreads")
-
-@spreads_router.get("/test")
-async def get_spreads(
-    spreads_calculator: spreads_calculator_dependency
-):
-    return await spreads_calculator.create(
-        PriceTickerRequest(
-            crypto_id='BTC/USDT',
-            interval="4h",
-            api_provider="binance"
-        )
-    )
 
 @spreads_router.post("/per-ticker/all")
 async def get_spreads(
