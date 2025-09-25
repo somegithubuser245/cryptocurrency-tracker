26e9cf9517075c4bd38a9f1ca6710e8b05cf6ad4
somegithubuser245 <New_username_001@protonmail.com>
2025-09-25 18:04:04 +0200

    add upsert functionality to exchange names table as well

--- backend/src/background/db/db_pairs.py
+++ backend/src/background/db/db_pairs.py
@@ -1,50 +1,38 @@
 import logging
 from typing import Tuple
 
-from domain.models import BatchStatus, CryptoPairName, SupportedExchangesByCrypto
+from domain.models import CryptoPairName, SupportedExchangesByCrypto
 from services.db_session import DBSessionDep
 from sqlalchemy import Row, Sequence, func, insert, select
 from sqlalchemy.dialects.postgresql import insert as upsert
 
 logger = logging.getLogger(__name__)
 
+
 def insert_or_update_pairs(pairs: list[str], session: DBSessionDep) -> None:
     """
     Inserts or updates pairs.
-    Returns list of all of the inserted ids
-
-    Note:
-    ----
-    Doesn't return ids of already existing pairs
     """
     db_readable_dict = [{"crypto_name": pair} for pair in pairs]
 
-    stmt = upsert(CryptoPairName)
-    stmt = stmt.on_conflict_do_nothing(index_elements=["crypto_name"])
-    crypto_ids = session.scalars(
-        stmt.returning(CryptoPairName.id),
-        db_readable_dict,
-        execution_options={"populate_existing": True},
-    ).all()
+    stmt = (
+        upsert(CryptoPairName)
+        .values(db_readable_dict)
+        .on_conflict_do_nothing(index_elements=["crypto_name"])
+    )
+    session.execute(stmt)
     session.commit()
-    return crypto_ids
 
 
-def insert_exchange_names(
-        exchange_name: str,
-        exchange_specific_symbols: list[str],
-        session: DBSessionDep
-    ) -> None:
+def insert_exchange_names(exchange_name: str, crypto_ids: list[str], session: DBSessionDep) -> None:
     """
     Inserts a row with a crypto id and the corresponding
     supported exchange
+
+    Makes use of postgres insert or update (upsert)
     """
     all_crypto_ids = (
-        session.execute(
-            select(CryptoPairName.id).where(
-                CryptoPairName.crypto_name.in_(exchange_specific_symbols)
-            )
-        )
+        session.execute(select(CryptoPairName.id).where(CryptoPairName.crypto_name.in_(crypto_ids)))
         .scalars()
         .all()
     )
@@ -54,12 +42,16 @@ def insert_exchange_names(
         for crypto_id in all_crypto_ids
     ]
     try:
-        stmt = insert(SupportedExchangesByCrypto).values(db_readable_dict)
+        stmt = (
+            upsert(SupportedExchangesByCrypto)
+            .values(db_readable_dict)
+            .on_conflict_do_nothing(constraint="unique_id_to_exchange")
+        )
         session.execute(stmt)
+        session.commit()
     except Exception as e:
         msg = f"Failed to insert exchange names for {exchange_name}"
         logger.exception(msg)
-    session.commit()
 
 
 def get_arbitrable_with_threshold(threshold: int, session: DBSessionDep) -> list[int]:
@@ -72,22 +64,23 @@ def get_arbitrable_with_threshold(threshold: int, session: DBSessionDep) -> list
     """
     stmt = (
         select(SupportedExchangesByCrypto.crypto_id)
-        .group_by(SupportedExchangesByCrypto.crypto_id,)
+        .group_by(
+            SupportedExchangesByCrypto.crypto_id,
+        )
         .having(func.count(SupportedExchangesByCrypto.crypto_id) >= threshold)
     )
     result = session.execute(stmt).scalars().all()
 
-    stmt = (
-        select(SupportedExchangesByCrypto.id)
-        .where(SupportedExchangesByCrypto.crypto_id.in_(result))
+    stmt = select(SupportedExchangesByCrypto.id).where(
+        SupportedExchangesByCrypto.crypto_id.in_(result)
     )
     result = session.execute(stmt).scalars().all()
     return list(result)
 
+
 def get_params_for_crypto_dto(
-        ids_list: list[int],
-        session: DBSessionDep
-    ) -> Sequence[Row[Tuple[int, str, str]]]:
+    ids_list: list[int], session: DBSessionDep
+) -> Sequence[Row[Tuple[int, str, str]]]:
     """
     Returns tuples in following order:
 
@@ -97,10 +90,10 @@ def get_params_for_crypto_dto(
         select(
             SupportedExchangesByCrypto.id,
             CryptoPairName.crypto_name,
-            SupportedExchangesByCrypto.supported_exchange
-        ).join(CryptoPairName,
-               CryptoPairName.id == SupportedExchangesByCrypto.crypto_id
-        ).where(SupportedExchangesByCrypto.id.in_(ids_list))
+            SupportedExchangesByCrypto.supported_exchange,
+        )
+        .join(CryptoPairName, CryptoPairName.id == SupportedExchangesByCrypto.crypto_id)
+        .where(SupportedExchangesByCrypto.id.in_(ids_list))
         # the order in which the ids are returned is very important
         # we must order by id to have different exchanges for an id first
         # otherwise we'll have all crypto pairs for one exchange only first
--- backend/src/domain/models.py
+++ backend/src/domain/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
@@ -24,6 +24,11 @@ class SupportedExchangesByCrypto(Base):
     crypto_id: Mapped[int] = mapped_column(ForeignKey("crypto_pairs_names.id"))
     supported_exchange: Mapped[str] = mapped_column(nullable=False, index=True)
     crypto_names: Mapped["CryptoPairName"] = relationship(back_populates="supported_exchanges")
+
+    __table_args__ = (
+        UniqueConstraint("crypto_id", "supported_exchange", name="unique_id_to_exchange"),
+    )
+
 
 class BatchStatus(Base):
     __tablename__ = "batch_status"
